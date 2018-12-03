ab17fb8ef65f76be5ffce461609db2193838039e
zhanghao <zhanghao@zhanghaodeMacBook-Pro.local>
2018-12-03 09:55:55 +0800

    build ubuntu image

--- ubuntu/start_container.sh
+++ ubuntu/start_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PWD=$(pwd)
-docker run --memory=4096M --memory-swap=4096M --security-opt seccomp=unconfined -v $PWD/code:/root/code --name ubuntu -it ubuntu:helayzhang bash
+docker run --memory=4096M --memory-swap=4096M --security-opt seccomp=unconfined -v /Users/zhanghao/codes:/root/codes --name ubuntu -it ubuntu:helayzhang /bin/bash
