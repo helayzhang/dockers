2d2cd62147fcae5779f5748f431afdc3908fad1e
helayzhang <helayzhang@gmail.com>
2018-12-03 14:45:10 +0800

    mount docker with share

--- ubuntu/start_container.sh
+++ ubuntu/start_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PWD=$(pwd)
-docker run --memory=4096M --memory-swap=4096M --security-opt seccomp=unconfined -v /Users/zhanghao/codes:/root/codes --name ubuntu -it ubuntu:helayzhang /bin/bash
+docker run --memory=4096M --memory-swap=4096M --security-opt seccomp=unconfined -v /Users/zhanghao/codes:/root/codes:delegated --name ubuntu -it ubuntu:helayzhang_0.1 /bin/zsh
