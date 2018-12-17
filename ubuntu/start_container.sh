9f6e7a6d124742593e1b7ffaed552c6b10325dfe
helayzhang <helayzhang@gmail.com>
2018-12-17 14:56:14 +0800

    add protobuf

--- ubuntu/start_container.sh
+++ ubuntu/start_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PWD=$(pwd)
-docker run --memory=4096M --memory-swap=4096M --security-opt seccomp=unconfined -v /Users/zhanghao/codes:/root/codes:delegated --name ubuntu -it helayzhang/ubuntu:1.1 /bin/zsh
+docker run --memory=4096M --memory-swap=4096M --security-opt seccomp=unconfined -v /Users/zhanghao/codes:/root/codes:delegated --name ubuntu -it helayzhang/ubuntu:1.2 /bin/zsh
