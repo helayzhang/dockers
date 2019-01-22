07baef07541d8a62f38431c42e7ff3aca265624a
helayzhang <helayzhang@gmail.com>
2019-01-22 18:53:00 +0800

    update new ubuntu start script

--- ubuntu/start_container.sh
+++ ubuntu/start_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PWD=$(pwd)
-docker run --memory=4096M --memory-swap=4096M --security-opt seccomp=unconfined -v /Users/zhanghao/codes:/root/codes:delegated --network host --name ubuntu -it helayzhang/ubuntu:1.4 /bin/zsh
+docker run --memory=4096M --memory-swap=4096M --security-opt seccomp=unconfined -v /Users/zhanghao/codes:/root/codes:delegated --network host --name ubuntu -it helayzhang/ubuntu:1.5 /bin/zsh
